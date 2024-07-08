19e98d1131d07fcee763ee5a1dc75e665c4938c0
Muhammad Anas Qureshi <anas.qureshi@puresquare>
2024-07-08 21:30:20 +0500

    updating script

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 echo "deploying helm charts"
 echo " "
 
-cd helm-mongodb/ && helm install -f values.yaml helm-mongodb . && cd ..
-cd helm-mongo-express/ && helm install -f values.yaml helm-mongo-express . && cd ..
-cd helm-hallucination/ && helm install -f values.yaml helm-hallucination . && cd ..
-cd helm-toxicity/ && helm install -f values.yaml helm-toxicity . && cd ..
-cd helm-privacy/ && helm install -f values.yaml helm-privacy . && cd ..
-cd helm-relevance/ && helm install -f values.yaml helm-relevance . && cd ..
+cd $(pwd)/helm-mongodb/ && helm install -f values.yaml helm-mongodb . && cd ..
+cd $(pwd)/helm-mongo-express/ && helm install -f values.yaml helm-mongo-express . && cd ..
+cd $(pwd)/helm-hallucination/ && helm install -f values.yaml helm-hallucination . && cd ..
+cd $(pwd)/helm-toxicity/ && helm install -f values.yaml helm-toxicity . && cd ..
+cd $(pwd)/helm-privacy/ && helm install -f values.yaml helm-privacy . && cd ..
+cd $(pwd)/helm-relevance/ && helm install -f values.yaml helm-relevance . && cd ..
 
 
 echo "Following Helm charts deployed"
